11c5f1601a0cc06e5419c524a0e1a28b2cef04a9
Craig West <iwswordpress@gmail.com>
2024-09-13 15:22:28 +0100

    refactor and combine

--- tests/mytest_class.py
+++ tests/mytest_class.py
@@ -23,29 +23,29 @@ class MyTestSample:
             f"\n[dark_orange italic]Running teardown for {method.__name__}[/]"
         )
 
-    def my_test_cls_50(self):
+    def my_test_50_add(self):
         actual_result = add(5, 2)
         expected_result = 7
         print(f"Actual result: {actual_result} - Expected result: {expected_result}")
         color_result(actual_result, expected_result)
         store_result(sys._getframe().f_code.co_name, actual_result, expected_result)
 
-    def my_test_cls_51(self):
-        actual_result = add(10, 10)
+    def my_test_51_add_fail(self):
+        actual_result = add(10, 10) + 1
         expected_result = 20
         print(f"Actual result: {actual_result} - Expected result: {expected_result}")
         color_result(actual_result, expected_result)
         store_result(sys._getframe().f_code.co_name, actual_result, expected_result)
 
-    def my_test_cls_52(self):
+    def my_test_52_add(self):
         actual_result = add(22, 10)
         expected_result = 32
         print(f"Actual result: {actual_result} - Expected result: {expected_result}")
         color_result(actual_result, expected_result)
         store_result(sys._getframe().f_code.co_name, actual_result, expected_result)
 
-    def my_test_cls_53(self):
-        actual_result = add(25, 45)
+    def my_test_53_add_fail(self):
+        actual_result = add(25, 45) + 1
         expected_result = 70
         print(f"Actual result: {actual_result} - Expected result: {expected_result}")
         color_result(actual_result, expected_result)
