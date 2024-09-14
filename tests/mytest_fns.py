d7d2f9637d77d223ece42be7ea6ab559fd5a8b43
Craig West <iwswordpress@gmail.com>
2024-09-14 15:16:20 +0100

    test data OK

--- tests/mytest_fns.py
+++ tests/mytest_fns.py
@@ -1,25 +1,25 @@
 from src.app import add
 
 
-def my_test_10_add():
-    actual_result = add(5, 2)
-    expected_result = 7
+def my_test_101_add():
+    actual_result = add(15, 20)
+    expected_result = 35
     assert (
         actual_result == expected_result
     ), f"Actual result: {actual_result} - Expected result: {expected_result}"
 
 
-def my_test_20_add():
-    actual_result = add(4, 2)
-    expected_result = 6
+def my_test_102_add():
+    actual_result = add(8, 4)
+    expected_result = 12
     assert (
         actual_result == expected_result
     ), f"Actual result: {actual_result} - Expected result: {expected_result}"
 
 
-def my_test_30_FAIL():
-    actual_result = add(4, 2) + 1
-    expected_result = 6
+def my_test_103_FAIL():
+    actual_result = add(7, 2) + 10
+    expected_result = 9
     assert (
         actual_result == expected_result
     ), f"Actual result: {actual_result} - Expected result: {expected_result}"
