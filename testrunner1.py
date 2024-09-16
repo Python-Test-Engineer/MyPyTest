3b0ecccc4ceb19e70c3ad0776b2b337e21273fe6
Craig West <iwswordpress@gmail.com>
2024-09-16 21:43:32 +0100

    testrunner

--- testrunner1.py
+++ testrunner1.py
@@ -1,13 +1,16 @@
 import importlib
-import inspect
+
+from tests.mytest_class import MyTestSample  # is src.add imported?
+
 from rich.console import Console
+from _mypytest.results import Results
 from _mypytest.utils import display_test_result
 from utils.read_mypytest import get_mytest_dir, get_mytest_files, get_version
-
-from _mypytest.results import Results
+from get import get_all_class_methods, get_all_classes
 
 r = Results.get_instance()
 console = Console()
+# get location of tests
 test_dir = get_mytest_dir()
 test_files = get_mytest_files()
 test_version = get_version()
@@ -16,48 +19,51 @@ console.print(f"[cyan]Test dirctory: [green bold]{test_dir}[/][/cyan]")
 console.print(f"[cyan]Test files: [green bold]{test_files}[/][/cyan]")
 console.print(f"[cyan]Test version: [green bold]{test_version}[/][/cyan]")
 console.print("")
-# Dynamically import the module
-module_name = (
-    "tests.mytest_class_01"  # replace with the name of the module you want to import
-)
-module = importlib.import_module(module_name)
 
-# Get all classes in the module
-classes = [cls for cls in module.__dict__.values() if inspect.isclass(cls)]
-
-# Iterate over each class and invoke all its methods
+console.print("[dark_orange]================ RUN CLASS TESTS ===================[/]")
+# console.print(globals())
+all_objs = dir(MyTestSample)
+method_names = [(str(obj)) for obj in all_objs if obj.startswith("my_test_")]
+# console.print(method_names)
+# # Dictionary to store function names as keys and functions as values
+method_names = get_all_class_methods("tests/mytest_class.py")
+classes = get_all_classes("tests/mytest_class.py")
+console.print("[green]Classes:[/]")
 for cls in classes:
-    console.print(f"[dark_orange]Class: {cls.__name__}[/]")
-    methods = inspect.getmembers(cls, inspect.isfunction)
-    methods = [method for method in methods if method[0].startswith("my_test")]
-    for method_name, method in methods:
-        console.print(f"[cyan]Test: {method_name}[/]")
-        try:
-            # Create an instance of the class
-            instance = cls()
-            # Get the method without the instance
-            method = getattr(cls, method_name)
-            # Call the method without the instance
-            method.__get__(None, cls)()
-            result = {
-                "test_name": method_name,
-                "test_result": "PASSED",
-                "test_message": None,
-            }
-            display_test_result(result, "PASSED")
-            r.add_result(result)
+    console.print(f"[cyan]{cls}[/]")
+test_class_dict = {}
 
-        except Exception as e:
-            result = {
-                "test_name": method_name,
-                "test_result": "FAILED",
-                "test_message": str(e),
-            }
-            display_test_result(result, "FAILED")
-            r.add_result(result)
-            print(e)
-        finally:
-            pass
+# # Populate the dictionary
+
+for method_name in method_names:
+    test_class_dict[method_name] = MyTestSample.__dict__[method_name]
+
+
+# console.print("test class dict", test_class_dict)
+console.print("[dark_orange]Running cls tests...[/dark_orange]")
+for test_class_name, class_method in test_class_dict.items():
+    console.print(f"[cyan]Test: {test_class_name}[/cyan]")
+    try:
+        class_method()
+        result = {
+            "test_name": test_class_name,
+            "test_result": "PASSED",
+            "test_message": None,
+        }
+        display_test_result(result, "PASSED")
+
+        r.add_result(result)
+    except Exception as e:
+        result = {
+            "test_name": test_class_name,
+            "test_result": "FAILED",
+            "test_message": str(e),
+        }
+        display_test_result(result, "FAILED")
+        r.add_result(result)
+        print(e)
+    finally:
+        pass
 
 console.print("\n[dark_orange]================ RUN FN TESTS ===================[/]")
 mod = "tests.mytest_fns"
@@ -95,9 +101,11 @@ for test_name, func in test_dict.items():
         }
         display_test_result(result, "FAILED")
         r.add_result(result)
-        print(e)
+
+        # print(e)
     finally:
         pass
+
 
 console.print("[green]================ Test Summary ===================[/]")
 r.get_result_totals()
