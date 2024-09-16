974ec0e4b681ea4332cf8ba05850fee35a7c7ca6
Craig West <iwswordpress@gmail.com>
2024-09-16 14:31:16 +0100

    progress on get tests

--- _mypytest/get_tests.py
+++ _mypytest/get_tests.py
@@ -1,47 +1,20 @@
 import importlib
 import pathlib
-from utils import display_test_result
 from rich.console import Console
 from results import Results
-from utils import display_test_result
+
 
 r = Results.get_instance()
 console = Console()
 location = pathlib.Path("./tests")
 
-# .rglob() produces a generator too
+
 all_test_files = list(location.glob("mytest_*.py"))
 console.print(all_test_files)
 
+# run tests
+for test_file in all_test_files:
+    # globals()[test_file] = importlib.import_module(test_file)
+    console.print("The module names are :", test_file)
 
-for test_name in all_test_files:
-    globals()[test_name] = importlib.import_module
-    func = globals()[test_name]
-    console.print(globals()[test_name])
-    console.print(f"[cyan]Test: {test_name}[/cyan]")
-    try:
-        func()
-        result = {
-            "test_name": func.__name__,
-            "test_result": "PASSED",
-            "test_message": None,
-        }
-        display_test_result(result, "PASSED")
-        r.add_result(result)
-    except Exception as e:
-        result = {
-            "test_name": func.__name__,
-            "test_result": "FAILED",
-            "test_message": str(e),
-        }
-        display_test_result(result, "FAILED")
-        r.add_result(result)
-
-        # print(e)
-    finally:
-        pass
-
-
-console.print("[green]================ Test Summary ===================[/]")
-r.get_result_totals()
-console.print("[green]=================================================[/]")
+console.print("Now import these modules...get fns...run them...save results")
