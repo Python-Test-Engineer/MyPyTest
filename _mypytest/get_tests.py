b65cf0c0bec14551209f145db03b780b45516fd2
Craig West <iwswordpress@gmail.com>
2024-09-16 16:53:18 +0100

    discovery

--- _mypytest/get_tests.py
+++ _mypytest/get_tests.py
@@ -1,20 +1,54 @@
+import os
 import importlib
+import fnmatch
 import pathlib
 from rich.console import Console
 from results import Results
+from read_mypytest import get_mytest_dir, get_mytest_files, get_version
+
+# main.py
+import os.path
+import types
+import sys
 
 
 r = Results.get_instance()
 console = Console()
-location = pathlib.Path("./tests")
+# get location of tests
+test_dir = get_mytest_dir()
+test_files = get_mytest_files()
+test_version = get_version()
+
+console.print("[dark_orange]================ HEADER ===================[/]")
+console.print(f"[cyan]Test dirctory: [green bold]{test_dir}[/][/cyan]")
+console.print(f"[cyan]Test files: [green bold]{test_files}[/][/cyan]")
+console.print(f"[cyan]Test version: [green bold]{test_version}[/][/cyan]")
+console.print("")
+
+all_test_files = [
+    f for f in os.listdir(test_dir) if os.path.isfile(os.path.join(test_dir, f))
+]
 
 
-all_test_files = list(location.glob("mytest_*.py"))
+all_test_files = [f for f in all_test_files if fnmatch.fnmatch(f, "mytest_*")]
+
 console.print(all_test_files)
 
-# run tests
-for test_file in all_test_files:
-    # globals()[test_file] = importlib.import_module(test_file)
-    console.print("The module names are :", test_file)
+module_file = all_test_files[0]
+console.print(module_file)
 
-console.print("Now import these modules...get fns...run them...save results")
+
+module_path = "./tests"
+
+module_rel_file_path = os.path.join(module_path, module_file)
+module_abs_file_path = os.path.abspath(module_rel_file_path)
+
+# read source code from file
+with open(module_rel_file_path, "r") as code_file:
+    source_code = code_file.read()
+
+# next we create a module object
+mod = types.ModuleType(module_file)
+
+console.print(mod)
+console.print(dir(mod))
---
+++ _mypytest/read_mypytest.py
@@ -0,0 +1,32 @@
+"""Reads mypytest.ini"""
+
+import configparser
+from pathlib import Path
+
+
+CG_FILE = "mypytest.ini"
+
+config = configparser.ConfigParser()
+BASE_DIR = Path(__file__).resolve().parent.parent
+CONFIG_FILE = BASE_DIR.joinpath(CG_FILE)
+config.read(CONFIG_FILE)
+
+
+def get_mytest_dir():
+    """GET"""
+    return config["tests"]["dir"]
+
+
+def get_mytest_files():
+    """GET"""
+    return config["tests"]["files"]
+
+
+def get_version():
+    return config["framework"]["version"]
+
+
+if __name__ == "__main__":
+
+    print(f"TEST_DIR: {get_mytest_dir()}")
+    print(f"VERSION: {get_version()}")
