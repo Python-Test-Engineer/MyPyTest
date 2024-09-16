89390b9b581a4dbb8cd45e3c2691d2e91886321d
Craig West <iwswordpress@gmail.com>
2024-09-16 21:49:43 +0100

    testrunner OK

--- tests/mytest_class_01.py
+++ tests/mytest_class_01.py
@@ -11,28 +11,28 @@ class MyTestSample:
 
         print(f"\n[dark_orange italic]Running teardown for {method.__name__}[/]")
 
-    def my_test_2628_cls_add_FAIL():
+    def my_test_001_cls_add_FAIL():
         actual_result = add(15, 20) + 20
         expected_result = 35
         assert (
             actual_result == expected_result
         ), f"Actual result: {actual_result} - Expected result: {expected_result}"
 
-    def my_test_2223_cls_add():
+    def my_test_002_cls_add():
         actual_result = add(15, 20)
         expected_result = 35
         assert (
             actual_result == expected_result
         ), f"Actual result: {actual_result} - Expected result: {expected_result}"
 
-    def my_test_2224_cls_add():
+    def my_test_003_cls_add():
         actual_result = add(10, 30)
         expected_result = 40
         assert (
             actual_result == expected_result
         ), f"Actual result: {actual_result} - Expected result: {expected_result}"
 
-    def my_test_2625_cls_add_FAIL():
+    def my_test_004_cls_add_FAIL():
         actual_result = add(22, 2) + 10
         expected_result = 24
         assert (
@@ -43,23 +43,62 @@ class MyTestSample:
 class MyTestSample2:
     # we can define setup and teardown methods as well
 
-    def my_test_301_cls_add():
+    def my_test_005_cls_add():
         actual_result = add(15, 20)
         expected_result = 35
         assert (
             actual_result == expected_result
         ), f"Actual result: {actual_result} - Expected result: {expected_result}"
 
-    def my_test_302_cls_add():
+    def my_test_006_cls_add():
         actual_result = add(10, 30)
         expected_result = 40
         assert (
             actual_result == expected_result
         ), f"Actual result: {actual_result} - Expected result: {expected_result}"
 
-    def my_test_303_cls_add_FAIL():
+    def my_test_007_cls_add_FAIL():
         actual_result = add(22, 2) + 10
         expected_result = 24
+        assert (
+            actual_result == expected_result
+        ), f"Actual result: {actual_result} - Expected result: {expected_result}"
+
+
+class MyTestSample3:
+    # we can define setup and teardown methods as well
+
+    def my_test_008_cls_add():
+        actual_result = add(15, 20)
+        expected_result = 35
+        assert (
+            actual_result == expected_result
+        ), f"Actual result: {actual_result} - Expected result: {expected_result}"
+
+    def my_test_009_cls_add():
+        actual_result = add(10, 30)
+        expected_result = 40
+        assert (
+            actual_result == expected_result
+        ), f"Actual result: {actual_result} - Expected result: {expected_result}"
+
+    def my_test_010_cls_add_FAIL():
+        actual_result = add(22, 2) + 10
+        expected_result = 24
+        assert (
+            actual_result == expected_result
+        ), f"Actual result: {actual_result} - Expected result: {expected_result}"
+
+    def my_test_011_cls_add():
+        actual_result = add(10, 30)
+        expected_result = 40
+        assert (
+            actual_result == expected_result
+        ), f"Actual result: {actual_result} - Expected result: {expected_result}"
+
+    def my_test_012_cls_add():
+        actual_result = add(10, 30)
+        expected_result = 40
         assert (
             actual_result == expected_result
         ), f"Actual result: {actual_result} - Expected result: {expected_result}"
--- tests/mytest_fns.py
+++ tests/mytest_fns.py
@@ -1,7 +1,7 @@
 from src.app import add
 
 
-def my_test_101_add():
+def my_test_013_add():
     actual_result = add(15, 20)
     expected_result = 35
     assert (
@@ -9,7 +9,7 @@ def my_test_101_add():
     ), f"Actual result: {actual_result} - Expected result: {expected_result}"
 
 
-def my_test_102_add():
+def my_test_014_add():
     actual_result = add(8, 4)
     expected_result = 12
     assert (
@@ -17,9 +17,41 @@ def my_test_102_add():
     ), f"Actual result: {actual_result} - Expected result: {expected_result}"
 
 
-def my_test_103_FAIL():
+def my_test_015_add():
+    actual_result = add(8, 4)
+    expected_result = 12
+    assert (
+        actual_result == expected_result
+    ), f"Actual result: {actual_result} - Expected result: {expected_result}"
+
+
+def my_test_016_add():
+    actual_result = add(8, 4)
+    expected_result = 12
+    assert (
+        actual_result == expected_result
+    ), f"Actual result: {actual_result} - Expected result: {expected_result}"
+
+
+def my_test_017_add():
+    actual_result = add(8, 4)
+    expected_result = 12
+    assert (
+        actual_result == expected_result
+    ), f"Actual result: {actual_result} - Expected result: {expected_result}"
+
+
+def my_test_018_FAIL():
     actual_result = add(7, 2) + 10
     expected_result = 9
+    assert (
+        actual_result == expected_result
+    ), f"Actual result: {actual_result} - Expected result: {expected_result}"
+
+
+def my_test_019_FAIL():
+    actual_result = add(17, 3) + 10
+    expected_result = 20
     assert (
         actual_result == expected_result
     ), f"Actual result: {actual_result} - Expected result: {expected_result}"
